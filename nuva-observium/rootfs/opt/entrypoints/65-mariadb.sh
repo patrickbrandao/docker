fe08a33a7872104f44ca1e99dda2bae5fa152dcb
Patrick Brandão <patrickbrandao@gmail.com>
2019-04-24 17:03:22 -0300

    Create 65-mariadb.sh

---
+++ nuva-observium/rootfs/opt/entrypoints/65-mariadb.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+
+
+initlogfile="/var/log/init.log"
+_log(){ now=$(date "+%Y-%m-%d-%T"); echo "$now|$@"; echo "$now|$@" >> $initlogfile; }
+_eval(){ _log "Running: $@"; out=$(eval "$@" 2>&1); sn="$?"; _log "Output[$sn]: $out"; }
+_logps(){ _log "Process list:"; (echo; ps aux; echo; ) >> $initlogfile; }
+
+
+# Preparar mariadb para execucao
+
+	[ "x$MARIADB_DATADIR" = "x" ] && {
+		MARIADB_DATADIR=/var/lib/mysql
+		export MARIADB_DATADIR=/var/lib/mysql
+	}
+
+	_log "Mariadb: Iniciando preparacao, datadir=$MARIADB_DATADIR"
+
+	mkdir -p /run/mariadb
+	chown mysql.mysql /run/mariadb
+
+	mkdir -p $MARIADB_DATADIR
+	chown mysql.mysql $MARIADB_DATADIR
+
+
+# Instalar banco de dados inicial
+    _idb=0
+    [ -d "$MARIADB_DATADIR/mysql" ] || _idb=1
+    [ -d "$MARIADB_DATADIR/test" ] || _idb=2
+    [ "$_idb" = "0" ] || {
+    	CMD="/usr/bin/mysql_install_db --user=mysql --datadir=$MARIADB_DATADIR"
+   		_log "Mariadb: Instalando base de dados inicial, $CMD"
+   		_eval "$CMD"
+    }
+
+# Rodar mariadb
+	_log "Mariadb: Iniciando instancia"
+	( ( /usr/bin/mysqld_safe --datadir=$MARIADB_DATADIR --pid-file=$MARIADB_PIDFILE ) 2>/dev/null 1>/dev/null & )
+
+    # Aguardar alguns segundos pelo boot
+    for i in 1 2 3 4 5; do
+        mysql -uroot -e 'show processlist' 2>/dev/null 1>/dev/null
+        [ "$?" = "0" ] && break
+        sleep 1
+    done
+    _eval "mysql -uroot -e 'show processlist'"
+
+
+# Servico no supervisor
+	mkdir -p /etc/supervisor/conf.d
+    ( echo '[program:mariadb]'; \
+        echo "command=/usr/bin/mysqld_safe --datadir=$MARIADB_DATADIR --pid-file=$MARIADB_PIDFILE"; \
+        echo 'autostart=true'; \
+        echo 'autorestart=true'; \
+        echo 'priority=20'
+        echo 'user=root'; \
+		echo 'startretries=999999'; \
+		echo 'startsecs=3'; \
+		echo 'stopwaitsecs=3'; \
+        echo 'stdout_logfile=/dev/stdout'; \
+        echo 'stdout_logfile_maxbytes=0'; \
+        echo 'stderr_logfile=/dev/stderr'; \
+        echo 'stderr_logfile_maxbytes=0'; \
+        echo 'priority=10'; \
+    ) > /etc/supervisor/conf.d/mariadb.conf; \
+
+
+# Logar processos em execucao para verificar se o MARIADB esta rodando
+    _logps
+
+
+
