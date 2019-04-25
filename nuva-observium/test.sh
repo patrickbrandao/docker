099f003e06897ee76fc2d79a8b3021ef43b06404
Patrick Brandão <patrickbrandao@gmail.com>
2019-04-25 00:27:31 -0300

    Update test.sh

--- nuva-observium/test.sh
+++ nuva-observium/test.sh
@@ -16,3 +16,18 @@
 # Rodar instancia de etste
 	docker run -d --restart=always -h nuva-observium --name=nuva-observium -p 8080:80 -p 2222:22 nuva-observium
 
+exit
+
+	# Criando Observium de dados persistentes:
+	mkdir -p /storage/observiun-01/rrd
+	mkdir -p /storage/observiun-01/logs
+	mkdir -p /storage/observiun-01/mariadb
+
+	docker run -d --restart=always -h nuva-observium-01 --name=nuva-observium-01 \
+	    -p 8080:80 -p 2222:22 \
+	    --mount type=bind,source=/storage/observiun-01/rrd,destination=/opt/observium/rrd,readonly=false \
+	    --mount type=bind,source=/storage/observiun-01/logs,destination=/opt/observium/logs,readonly=false \
+	    --mount type=bind,source=/storage/observiun-01/mariadb,destination=/var/lib/mysql,readonly=false \
+	    nuva-observium
+
+
